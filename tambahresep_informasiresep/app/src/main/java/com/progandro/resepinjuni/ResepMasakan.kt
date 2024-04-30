a1bc93d241b3e72168562c9aec40d543ec248284
artapages <juniarta@ti.ukdw.ac.id>
2024-04-30 17:30:20 +0700

    tambah resep

---
+++ tambahresep_informasiresep/app/src/main/java/com/progandro/resepinjuni/ResepMasakan.kt
@@ -0,0 +1,11 @@
+package com.progandro.resepinjuni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResepMasakan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resep_masakan)
+    }
+}
