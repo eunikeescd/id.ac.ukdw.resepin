29cc67cbfac1f1304cee10e7c6daaec99aaee950
artapages <93313578+artapages@users.noreply.github.com>
2024-04-30 17:24:30 +0700

    Add files via upload

---
+++ tersimpan_pengaturan_ubahprofile/app/src/main/java/com/example/resepmakanan/pengaturan.kt
@@ -0,0 +1,53 @@
+package com.example.resepmakanan;
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class pengaturan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pengaturan)
+
+        val linearButton: LinearLayout = findViewById(R.id.ubah)
+        linearButton.setOnClickListener {
+            // Buat Intent untuk navigasi ke halaman baru (NewActivity)
+            val intent = Intent(this, ubahprofil::class.java)
+            startActivity(intent)
+        }
+        val linearButton2: LinearLayout = findViewById(R.id.keluar)
+        linearButton2.setOnClickListener {
+            // Menampilkan dialog konfirmasi sebelum logout
+            showLogoutDialog()
+        }
+    }
+
+    private fun showLogoutDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout")
+        builder.setMessage("Apakah Anda yakin ingin logout?")
+
+        builder.setPositiveButton("Ya") { dialogInterface: DialogInterface, i: Int ->
+            // Kode untuk logout, misalnya menghapus data login dan kembali ke halaman login
+            logout()
+        }
+
+        builder.setNegativeButton("Tidak") { dialogInterface: DialogInterface, i: Int ->
+            dialogInterface.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+    private fun logout() {
+        // Kode untuk logout, misalnya menghapus data login dan kembali ke halaman login
+        // Contoh sederhana, kembali ke halaman login (MainActivity)
+        val intent = Intent(this, ubahprofil::class.java)
+        startActivity(intent)
+        finish() // Menutup activity ini agar tidak bisa kembali ke MainActivity
+    }
+}
+
---
+++ tersimpan_pengaturan_ubahprofile/app/src/main/java/com/example/resepmakanan/tersimpan.kt
@@ -0,0 +1,13 @@
+package com.example.resepmakanan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class tersimpan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tersimpan)
+    }
+}
